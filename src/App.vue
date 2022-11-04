04d6486a1738c49a33afe37b1296909cee76fc5a
Yasuomvp <teemovv@outlook.com>
2022-11-04 11:26:15 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+     {{store.helloPinia}}
+     {{store.storeMap.get("holy")}}
+  </div>
+  <div>
+    <button @click="handleClick">store test</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { mainStore } from './stores'
+const store = mainStore()
+
+const handleClick = ()=>{
+  console.log('hss')
+  store.helloPinia = "change"
+  store.storeMap.set("holy","shit")
+
+}
+
+</script>
